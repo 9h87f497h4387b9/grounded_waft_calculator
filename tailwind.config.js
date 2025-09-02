70f6e379395f1c730ba17f1a1dfe123b2b04de55
Aaron Gadberry <aaron@gadberry.com>
2025-09-02 18:47:24 -0500

    Simplify TailwindCSS `content` paths by removing redundant entry.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 export default {
     content: [
         './index.html',
-        './**/*.{js,ts,jsx,tsx}', // This covers all your TSX files
-        './*.{js,ts,jsx,tsx}',
+        './**/*.{js,ts,jsx,tsx}' // This covers all your TSX files
     ],
     theme: {
         extend: {},
